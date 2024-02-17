ff179579c8eedd451309b15aaeaeb93f7a02f293
Gerben Droogers <gerben@droogers.eu>
2024-02-17 11:43:56 +0100

    Git initial

---
+++ src/main/java/app/ister/server/IsterServerApplication.java
@@ -0,0 +1,13 @@
+package app.ister.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsterServerApplication.class, args);
+	}
+
+}
