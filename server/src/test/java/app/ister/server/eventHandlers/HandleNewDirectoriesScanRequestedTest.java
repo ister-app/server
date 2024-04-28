fc3f8c55be34b749a59c0b4e6c27d5b937d7a6c3
Gerben Droogers <gerben@droogers.eu>
2024-04-28 13:48:02 +0200

    Tests

---
+++ server/src/test/java/app/ister/server/eventHandlers/HandleNewDirectoriesScanRequestedTest.java
@@ -0,0 +1,21 @@
+package app.ister.server.eventHandlers;
+
+import app.ister.server.enums.EventType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandleNewDirectoriesScanRequestedTest {
+    private HandleNewDirectoriesScanRequested subject;
+
+    @BeforeEach
+    void setUp() {
+        subject = new HandleNewDirectoriesScanRequested();
+    }
+
+    @Test
+    void handles() {
+        assertEquals(EventType.NEW_DIRECTORIES_SCAN_REQUEST, subject.handles());
+    }
+}
---
+++ server/src/test/java/app/ister/server/eventHandlers/HandleSubtitleFileFoundTest.java
@@ -0,0 +1,12 @@
+package app.ister.server.eventHandlers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandleSubtitleFileFoundTest {
+
+    @Test
+    void handles() {
+    }
+}
