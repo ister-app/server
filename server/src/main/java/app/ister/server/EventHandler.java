4188ade2d784c26de52d6bc65c8c2353c67b3d60
tijder <tijder@users.noreply.github.com>
2024-05-05 22:34:59 +0200

    Transcoding pause (#3)

--- server/src/main/java/app/ister/server/EventHandler.java
+++ server/src/main/java/app/ister/server/EventHandler.java
@@ -2,23 +2,14 @@ package app.ister.server;
 
 import app.ister.server.enums.EventType;
 import app.ister.server.eventHandlers.Handle;
-import app.ister.server.eventHandlers.HandleEpisodeFound;
-import app.ister.server.eventHandlers.HandleMediaFileFound;
-import app.ister.server.eventHandlers.HandleNewDirectoriesScanRequested;
-import app.ister.server.eventHandlers.HandleNfoFileFound;
-import app.ister.server.eventHandlers.HandleShowFound;
-import app.ister.server.eventHandlers.HandleSubtitleFileFound;
 import app.ister.server.repository.ServerEventRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
--- server/src/main/java/app/ister/server/controller/PlayQueueController.java
+++ server/src/main/java/app/ister/server/controller/PlayQueueController.java
@@ -2,16 +2,9 @@ package app.ister.server.controller;
 
 import app.ister.server.entitiy.PlayQueueEntity;
 import app.ister.server.entitiy.PlayQueueItemEntity;
-import app.ister.server.entitiy.UserEntity;
-import app.ister.server.entitiy.WatchStatusEntity;
-import app.ister.server.repository.EpisodeRepository;
-import app.ister.server.repository.PlayQueueRepository;
-import app.ister.server.repository.WatchStatusRepository;
-import app.ister.server.service.UserService;
-import app.ister.server.service.WatchStatusService;
+import app.ister.server.service.PlayQueueService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -21,66 +14,23 @@ import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
 public class PlayQueueController {
     @Autowired
-    private PlayQueueRepository playQueueRepository;
-
-    @Autowired
-    private EpisodeRepository episodeRepository;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private WatchStatusRepository watchStatusRepository;
-
-    @Autowired
-    private WatchStatusService watchStatusService;
+    private PlayQueueService playQueueService;
 
     @PreAuthorize("hasRole('user')")
     @MutationMapping
-    public PlayQueueEntity createPlayQueue(@Argument UUID showId, Authentication authentication) {
-        log.debug("Creating play queue for user: {}, show: {}", authentication.getName(), showId);
-        List<PlayQueueItemEntity> items = episodeRepository.findIdsOnlyByShowEntityId(
-                        UUID.fromString(showId.toString()),
-                        Sort.by("SeasonEntityNumber").ascending().and(
-                                Sort.by("number").ascending())).stream()
-                .map(idOnly -> PlayQueueItemEntity.builder().id(UUID.randomUUID()).itemId(idOnly.getId()).build()).collect(Collectors.toList());
-        UserEntity userEntity = userService.getOrCreateUser(authentication);
-        var playQueueEntity = PlayQueueEntity.builder().userEntity(userEntity).items(items).build();
-        playQueueRepository.save(playQueueEntity);
-        return playQueueEntity;
+    public PlayQueueEntity createPlayQueue(@Argument UUID showId, @Argument UUID episodeId, Authentication authentication) {
+        return playQueueService.createPlayQueue(showId, episodeId, authentication);
     }
 
     @PreAuthorize("hasRole('user')")
     @MutationMapping
     public Boolean updatePlayQueue(@Argument UUID id, @Argument long progressInMilliseconds, @Argument UUID playQueueItemId, Authentication authentication) {
-        log.debug("Updating play queue for user: {}", authentication.getName());
-        // Update the current playing episode
-        playQueueRepository.findById(id).ifPresent(playQueueEntity -> {
-            playQueueEntity.getItems().stream().filter(item -> item.getId().equals(playQueueItemId)).findAny().ifPresent(playQueueItemEntity -> {
-                playQueueEntity.setCurrentItem(playQueueItemEntity.getId());
-                playQueueRepository.save(playQueueEntity);
-                // Update the watch status of an episode if it's played for more then one minute
-                if (progressInMilliseconds > 60000) {
-                    episodeRepository.findById(playQueueItemEntity.getItemId()).ifPresent(episodeEntity -> {
-                        WatchStatusEntity watchStatusEntity = watchStatusService.getOrCreate(authentication, playQueueItemId, episodeEntity);
-                        watchStatusEntity.setProgressInMilliseconds(progressInMilliseconds);
-                        if (!episodeEntity.getMediaFileEntities().isEmpty()) {
-                            long durationOfMediaFile = episodeEntity.getMediaFileEntities().get(0).getDurationInMilliseconds();
-                            boolean durationIsLessThenOneMinute = durationOfMediaFile - progressInMilliseconds < 60000;
-                            watchStatusEntity.setWatched(durationIsLessThenOneMinute);
-                        }
-                        watchStatusRepository.save(watchStatusEntity);
-                    });
-                }
-            });
-        });
-        return true;
+        return playQueueService.updatePlayQueue(id, progressInMilliseconds, playQueueItemId, authentication);
     }
 
     @SchemaMapping(typeName = "PlayQueue", field = "playQueueItems")
