55f57696b9941854181d42e0898a4c03847caca6
Gerben Droogers <gerben@droogers.eu>
2025-05-31 09:50:32 +0200

    Serverinfo controller

---
+++ server/src/main/java/app/ister/server/controller/ServerInfoController.java
@@ -0,0 +1,51 @@
+package app.ister.server.controller;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Slf4j
+@Controller
+public class ServerInfoController {
+
+    @Value("${app.ister.server.name}")
+    private String serverName;
+
+    @Value("${app.ister.server.url}")
+    private String serverUrl;
+
+    @Value("${springdoc.oAuthFlow.openIdConnectUrl}")
+    private String openIdConnectUrl;
+
+    @QueryMapping
+    public ServerInfo getServerInfo() {
+        return ServerInfo.builder()
+                .name(serverName)
+                .url(serverUrl)
+                .version(getImplementationVersion())
+                .openIdUrl(openIdConnectUrl)
+                .build();
+    }
+
+    private String getImplementationVersion() {
+        return Optional.ofNullable(getClass().getPackage().getImplementationVersion())
+                .orElse("?.?.?-DEVELOPMENT");
+    }
+
+
+    @EqualsAndHashCode
+    @Getter
+    @Builder
+    public static class ServerInfo {
+        private String name;
+        private String url;
+        private String version;
+        private String openIdUrl;
+    }
+}
